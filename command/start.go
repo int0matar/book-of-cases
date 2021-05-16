07752f09224825e4121c7739d43b306eb3f86e1a
Roman B <intomatar@gmail.com>
2021-05-16 18:35:57 +0600

    Add connection to the database

--- command/start.go
+++ command/start.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"log"
+	"os"
 
 	todo "github.com/int0matar/case-book"
 	"github.com/int0matar/case-book/package/handler"
+	"github.com/int0matar/case-book/package/repository"
+	"github.com/int0matar/case-book/package/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+		Host:     os.Getenv("DB_HOSTNAME"),
+		Port:     os.Getenv("DB_PORT"),
+		SSLMode:  os.Getenv("DB_MODE"),
+	})
+
+	if err != nil {
+		log.Fatalf("database configuration initialization error: %s", err.Error())
+	}
+
+	repositories := repository.NewRepository(db)
+	services := service.NewService(repositories)
+	handlers := handler.NewHandler(services)
 
 	server := new(todo.Server)
-	if err := server.Run("8000", handler.InitRoutes()); err != nil {
-		log.Fatalf("An error occurred while the server was starting: %s", err.Error())
+	if err := server.Run(os.Getenv("LCPR_PORT"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("an error occurred while the server was starting: %s", err.Error())
 	}
 }
